d66948fd15468bd53e31e21a862538c34b4589d7
swexbe <swexbe@yahoo.se>
2017-08-18 11:15:07 +0200

    true lines

--- Chat/plot2d.py
+++ Chat/plot2d.py
@@ -10,11 +10,14 @@ screen = pygame.display.set_mode(size)
 ball = pygame.image.load("ball.bmp")
 ballrect = ball.get_rect()
 
+
 while 1:
+    yi = float(0)
     for event in pygame.event.get():
         if event.type == pygame.QUIT: sys.exit()
     for i in range(640):
-        y = i-320
-        pygame.draw.line(screen, [9,205,218], [i,480-(pow(y/24,2)*24)] , [i,480-(pow(y/24,2)*24)], 1)
+        y = float(i-320)
+        pygame.draw.line(screen, [9,205,218], [i,480-(pow(y/24,2)*24)] , [i-1,480-(pow(yi/24,2)*24)], 1)
+        yi = y
 
     pygame.display.flip()
