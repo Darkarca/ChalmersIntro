e906cde91aa793d4cf965f6958dc9be0ddfc63be
Darkarca <erik.gunnarsson98@gmail.com>
2017-08-18 10:46:04 +0200

    int till float, flytta upp y från -240 till0

--- Chat/plot2d.py
+++ Chat/plot2d.py
@@ -13,7 +13,8 @@ ballrect = ball.get_rect()
 while 1:
     for event in pygame.event.get():
         if event.type == pygame.QUIT: sys.exit()
-
-    pygame.draw.line(screen, [9,205,218], [0,0] , [100,100], 1)
+    for i in range(640):
+        y = i-320
+        pygame.draw.line(screen, [9,205,218], [i,480-(pow(y/24,2)*24)] , [i,480-(pow(y/24,2)*24)], 1)
 
     pygame.display.flip()
