5d4cfcf46a5fecc5dce04790685e8a05c201f32c
Darkarca <erik.gunnarsson98@gmail.com>
2017-08-22 10:19:44 +0200

    flera funktioner

--- Chat/plot2d.py
+++ Chat/plot2d.py
@@ -1,29 +1,40 @@
-import sys, pygame
+import sys, pygame, math
 
-function = str(raw_input("Enter function: "))
-function = function.replace("x", "x/24")
-function3 = function.replace("x", "xi")
-print function
-func2 = "pygame.draw.line(screen, [9,205,218], [i,240-("+function+")*24] , [i-1,240-("+function3+")*24], 1)"
+functions = []
+functions_x = []
+functions_xi = []
+exec_functions = []
+colors = []
+
+functioncount = raw_input("How many functions would you like to plot? ")
+
+for n in range(int(functioncount)):
+    functions.append(str(raw_input("Enter function: ")))
+    colors.append(raw_input("R, G, B: "))
+
+for func in range(len(functions)):
+    functions_x.append(functions[func].replace("x", "x/24"))
+    functions_xi.append(functions[func].replace("x", "xi/24"))
+    exec_functions.append("pygame.draw.line(screen, ["+colors[func]+"], [i,240-("+functions_x[func]+")*24] , [i-1,240-("+functions_xi[func]+")*24], 3)")
+
 pygame.init()
 
 size = width, height = 480, 480
 
 screen = pygame.display.set_mode(size)
-back = pygame.image.load("back2.gif")
+back = pygame.image.load("back3.gif")
 backrect = back.get_rect()
 screen.blit(back,backrect)
+
 while 1:
     xi = float(0)
     for event in pygame.event.get():
         if event.type == pygame.QUIT: sys.exit()
     for i in range(480):
         x = float(i-240)
-        #xstart = 240-(pow(x/24,2)*24)
-        #xend = 240-(pow(xi/24,2)*24)
-        #pygame.draw.line(screen, [9,205,218], [i,xstart] , [i-1,xend], 1)
-
-        exec(func2)
+        for function in exec_functions:
+            exec(function)
         xi = x
+        pygame.draw.rect(screen, [9, 205, 218], [0, 0, 480, 480], 3)
 
     pygame.display.flip()
