a2e60f00caed5adb006b9361db31c87acfdeba26
swexbe <swexbe@yahoo.se>
2017-08-24 11:30:01 +0200

    lejfes

--- Chat/Tetris.py
+++ Chat/Tetris.py
@@ -1,19 +1,19 @@
 import sys, pygame, math
 
 
-speed = 1
+speed = 500
 pygame.init()
 
 size = width, height = 360, 720
-
+dontburncpu = pygame.time.Clock()
 screen = pygame.display.set_mode(size)
 
 while 1:
     xi = float(0)
     for event in pygame.event.get():
         if event.type == pygame.QUIT: sys.exit()
-
-        sleep(speed)
+        print ("test")
+        dontburncpu.tick(30)
 
 
 
