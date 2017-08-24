01421275f5a0f0eda6e8c634fd62eed2d5fb3db2
Darkarca <erik.gunnarsson98@gmail.com>
2017-08-24 11:28:18 +0200

    music

--- Chat/Tetris.py
+++ Chat/Tetris.py
@@ -1,19 +1,20 @@
 import sys, pygame, math
 
 
-speed = 1
+speed = 2
 pygame.init()
 
 size = width, height = 360, 720
 
 screen = pygame.display.set_mode(size)
-
+pygame.mixer.music.load("tetris-gameboy-02.mp3")
+pygame.mixer.music.play(loops=-1, start=0.0)
 while 1:
     xi = float(0)
     for event in pygame.event.get():
         if event.type == pygame.QUIT: sys.exit()
 
-        sleep(speed)
+       # sleep(speed)
 
 
 
