d01cecf2b341447f087f1914cd7dd30004c8eead
swexbe <swexbe@yahoo.se>
2017-08-24 13:23:31 +0200

    iaiosnd

--- Chat/Tetris.py
+++ Chat/Tetris.py
@@ -8,9 +8,11 @@ size = width, height = 360, 720
 dontburncpu = pygame.time.Clock()
 screen = pygame.display.set_mode(size)
 blockisselected = False
-blockpos = []
-slectedpos = [0, 0]
-slectedpos.append([0, 0])
+blockposx = []
+blockposy = []
+slectedposx = []
+selectedposy = []
+slectedposx.append(0)
 pygame.mixer.music.load("tetris-gameboy-02.mp3")
 pygame.mixer.music.play(loops=-1, start=0.0)
 
@@ -18,11 +20,18 @@ while 1:
     if(blockisselected == False):
             rand = random.randint(1,1) #1,7
             if(rand == 1):
-                print()
+                slectedposx[1] = 4
+                slectedposx[2] = 5
+                slectedposx[3] = 6
+                slectedposx[4] = 7
+                slectedposy[1] = 1
+                slectedposy[2] = 1
+                slectedposy[3] = 1
+                slectedposy[4] = 1
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT: sys.exit()
-        print (slectedpos[1])
+        print (slectedposx[0])
         dontburncpu.tick(30)
 
 
