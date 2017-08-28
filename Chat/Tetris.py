cabda7ba347a6393445e9e155c12dd6759ebe71c
swexbe <swexbe@yahoo.se>
2017-08-28 08:08:14 +0200

    week1

--- Chat/Tetris.py
+++ Chat/Tetris.py
@@ -10,31 +10,40 @@ screen = pygame.display.set_mode(size)
 blockisselected = False
 blockposx = []
 blockposy = []
-slectedposx = []
-selectedposy = []
+slectedposx = [0, 1, 2, 3, 4]
+slectedposy = [0, 1, 2, 3, 4]
+blocks = []
 slectedposx.append(0)
 pygame.mixer.music.load("tetris-gameboy-02.mp3")
 pygame.mixer.music.play(loops=-1, start=0.0)
+BlueBlock = pygame.image.load("BlueBlock.png")
+BlueBlockRect = BlueBlock.get_rect()
 
 while 1:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT: sys.exit()
     if(blockisselected == False):
             rand = random.randint(1,1) #1,7
             if(rand == 1):
-                slectedposx[1] = 4
-                slectedposx[2] = 5
-                slectedposx[3] = 6
-                slectedposx[4] = 7
-                slectedposy[1] = 1
-                slectedposy[2] = 1
-                slectedposy[3] = 1
-                slectedposy[4] = 1
-
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT: sys.exit()
-        print (slectedposx[0])
-        dontburncpu.tick(30)
+                #slectedposx[1] = 4
+                #slectedposx[2] = 5
+                #slectedposx[3] = 6
+                #slectedposx[4] = 7
+                #slectedposy[1] = 1
+                #slectedposy[2] = 1
+                #slectedposy[3] = 1
+                #slectedposy[4] = 1
 
 
 
+    for i in range(0,3):
+        screen.blit(BlueBlock, BlueBlockRect)
 
-pygame.display.flip()
+
+
+    #print (slectedposx[0])
+    dontburncpu.tick(30)
+    pygame.display.flip()
+
+
+
